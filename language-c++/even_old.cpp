06c29b31e4dbcb578880b0e48210a711f9b986a0
NUH <144234496+methyle-acetate@users.noreply.github.com>
2023-09-14 15:48:07 +0500

    Add files via upload

---
+++ language-c++/even_old.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	int num;
+	cout<< "please enter a number"<< endl;
+	cin>> num;
+	if(num%2 == 0){
+		cout<< " The given number is even"<<endl;
+	}else{
+		cout<< "The given number is old";
+	}
+	return 0;
+}
