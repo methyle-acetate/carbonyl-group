84279d470b6a10eb85ffedcd56116830a23ccb08
NUH <144234496+methyle-acetate@users.noreply.github.com>
2023-09-19 16:19:25 +0500

    Create shape#1st using nested loops.cpp

---
+++ language-c++/shape#1st using nested loops.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+int main(){
+	int k,l;
+	for(k=5;k>=1;k--){
+		for(l=1;l<=k;l++){
+			cout << "*";
+		}
+		cout<< "\n";
+	}
+
+
+	return 0;
+}
